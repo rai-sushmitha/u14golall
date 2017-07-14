fd670a2767abea3356285c6977d6f600d1d7c6a7
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-14 14:07:35 +0530

    fixes indentation

--- go_install.sh
+++ go_install.sh
@@ -5,7 +5,7 @@
 export  CGO_ENABLED=0
 
 
- gvm install go1.4 && gvm use go1.4 && go install -a std && go get -u github.com/tools/godep
+gvm install go1.4 && gvm use go1.4 && go install -a std && go get -u github.com/tools/godep
 
 
 declare -a versions=( '1.7.6' '1.8.3' )
